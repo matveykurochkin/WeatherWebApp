69e1a8cb5c7eca606747bce20568bd5e9cbf8a3e
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-03-24 18:08:47 +0300

    update response from weather API

--- WeatherWebApp/WeatherWebApp/Models/WeatherResponse.cs
+++ WeatherWebApp/WeatherWebApp/Models/WeatherResponse.cs
@@ -1,10 +1,19 @@
 ﻿namespace WeatherWebApp.Models
 {
-    public class WeatherResponse
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Clouds
     {
         public int all { get; set; }
+    }
+
+    public class Coord
+    {
         public double lon { get; set; }
         public double lat { get; set; }
+    }
+
+    public class Main
+    {
         public double temp { get; set; }
         public double feels_like { get; set; }
         public double temp_min { get; set; }
@@ -13,18 +22,42 @@
         public int humidity { get; set; }
         public int sea_level { get; set; }
         public int grnd_level { get; set; }
+    }
+
+    public class Root
+    {
+        public Coord? coord { get; set; }
+        public List<Weather>? weather { get; set; }
         public string? @base { get; set; }
+        public Main? main { get; set; }
         public int visibility { get; set; }
+        public Wind? wind { get; set; }
+        public Clouds? clouds { get; set; }
         public int dt { get; set; }
+        public Sys? sys { get; set; }
         public int timezone { get; set; }
         public int id { get; set; }
         public string? name { get; set; }
         public int cod { get; set; }
+    }
+
+    public class Sys
+    {
         public string? country { get; set; }
         public int sunrise { get; set; }
         public int sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string? main { get; set; }
         public string? description { get; set; }
         public string? icon { get; set; }
+    }
+
+    public class Wind
+    {
         public double speed { get; set; }
         public int deg { get; set; }
         public double gust { get; set; }
